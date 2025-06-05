131d5297e625dda467023260e4866712c7eb8364
MrBogdan007 <bogdanbrohovetsky@gmail.com>
2025-06-05 21:00:02 +0300

    Commit at the :  21:00:02.55

--- Threads/src/RunTester.java
+++ Threads/src/RunTester.java
@@ -1,19 +1,28 @@
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class RunTester {
     public static void main(String[] args) {
         Runnable threadJob = new MyRunnable();
         ExecutorService threadExecutor = Executors.newSingleThreadExecutor();
-        threadExecutor.execute(threadJob);
-        System.out.println(Thread.currentThread().getName()+": back in main");
-        tellStory();
-        Thread.dumpStack();
+        CountDownLatch latch = new CountDownLatch(1);
+        threadExecutor.execute(() -> waitForLatchThenPrint(latch));
+        System.out.println("back in main");
+        latch.countDown();
         threadExecutor.shutdown();
     }
 
-    private static void tellStory() {
-        System.out.println("Story");
+    private static void waitForLatchThenPrint(CountDownLatch latch) {
+        try {
+            latch.await();
+            System.out.println("Top of the stack");
+        }catch(InterruptedException ex){
+            ex.printStackTrace();
+        }
+
     }
+
 
 }
